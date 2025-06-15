54c584f96e8050c6bee8b66eeb7f90b4da2a3e0f
dev-vin-dev <vincentmusyimi40@gmail.com>
2025-06-15 15:58:51 +0300

    Quiz question & ANswer

---
+++ quiz.js
@@ -0,0 +1,25 @@
+//Defining the function
+function checkAnswer() {
+    const correctAnswer = "4";
+    // Get the users selected radio
+    const selectedOption = document.querySelector('input[name="quiz"]:checked');
+
+    //Check if any option was selected
+    if (!selectedOption){
+        document.getElementById("feedback").textContent ="Please select and answer.";
+        return;
+    }
+
+    const userAnswer = selectedOption.value
+    //complaring answers
+    if (userAnswer===correctAnswer){
+        document.getElementById("feedback").textContent="Correct! Well done.";
+    
+    } else {
+        document.getElementById("feedback").textContent="That's not correct";
+    }
+}
+
+//Attach event listener to the button
+const submitButton = document.getElementById("submit-answer");
+submitButton.addEventListener("click", checkAnswer);
